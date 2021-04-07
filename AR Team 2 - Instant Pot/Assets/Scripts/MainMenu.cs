70bb215e4eef7d7bca8744f6a7066abfb7de001b
gjarnos <gjarnos@gmail.com>
2021-04-07 15:54:24 -0400

    Revert "More changes for UI"
    This reverts commit b0b4dd303e08bff304b797d977be10deb2216495.

--- AR Team 2 - Instant Pot/Assets/Scripts/MainMenu.cs
+++ AR Team 2 - Instant Pot/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void LoadARScene()
     {
-        SceneManager.LoadScene("ARScene v1.3");
+        SceneManager.LoadScene("ARScene v1.0");
     }
 }
