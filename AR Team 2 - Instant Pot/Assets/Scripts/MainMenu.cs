b0b4dd303e08bff304b797d977be10deb2216495
gjarnos <gjarnos@gmail.com>
2021-04-07 15:51:22 -0400

    More changes for UI
    Added blur.
    Changed font.
    Edited steps text.
    Modified the main menu screen.

--- AR Team 2 - Instant Pot/Assets/Scripts/MainMenu.cs
+++ AR Team 2 - Instant Pot/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void LoadARScene()
     {
-        SceneManager.LoadScene("ARScene v1.0");
+        SceneManager.LoadScene("ARScene v1.3");
     }
 }
